5647748e8dbbd34d0200de174089913082b19c14
Kuba <kubapaj00@gmail.com>
2020-09-20 23:24:20 +0200

    improved solution

--- Day2/script.py
+++ Day2/script.py
@@ -1,22 +1,7 @@
-# PART ONE
 with open('input.txt', 'r') as file:
     input = file.read().split(',')
 
 opcodes = [int(x) for x in input]
-opcodes[1] = 12
-opcodes[2] = 2
-pos = 0
-while pos <= len(opcodes):
-    if opcodes[pos] == 99:
-        break
-    elif opcodes[pos] == 1:
-        opcodes[opcodes[pos + 3]] = opcodes[opcodes[pos + 1]] + opcodes[opcodes[pos + 2]]
-    elif opcodes[pos] == 2:
-        opcodes[opcodes[pos + 3]] = opcodes[opcodes[pos + 1]] * opcodes[opcodes[pos + 2]]
-    pos += 4
-print(opcodes[0])
-
-# PART TWO
 
 for noun in range(100):
     for verb in range(100):
@@ -32,8 +17,11 @@ for noun in range(100):
             elif opcodes[pos] == 2:
                 opcodes[opcodes[pos + 3]] = opcodes[opcodes[pos + 1]] * opcodes[opcodes[pos + 2]]
             pos += 4
+        if (noun, verb) == (12, 2):
+            result_1 = opcodes[0]
         if opcodes[0] == 19690720:
-            result = 100 * noun + verb
+            result_2 = 100 * noun + verb
             break
 
-print(result)
+print("Result 1: ", result_1)
+print("Result 2: ", result_2)
